8e3b7c8b656b337f3a79b4e2315a332c5f85f5a5
Alexander-T2 <89840636+Alexander-T2@users.noreply.github.com>
2022-09-24 22:11:52 +0300

    abstract shape

---
+++ 10_3 Figures/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_3_Figures
+{
+    abstract class Shape
+    {
+        protected static int R = 15;
+        protected int x, y;
+        protected static string drawingColor;
+        static Shape()
+        {
+            // fill up?
+        }
+        public Shape(int x, int y)
+        {
+            // fill up?
+        }
+        abstract public bool IsInside();
+        abstract public void Draw();
+    }
+}
